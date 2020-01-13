47a1c1a7d19dae4322f641ba27c14bcbac5e2126
Bernadette Bizer <bbizer@ITA-5515.local>
2020-01-12 20:37:36 -0500

    add style elements and give a choice between listing courses, groups, or both

--- application.php
+++ application.php
@@ -10,7 +10,7 @@ function user_api_information_form_validate(){
 		print("UID, Key, and Secret are all required fields");
 		exit;
 	} else {
-		user_api_information_form_submit($uid, $key, $secret);
+		user_api_information_form_submit($uid, $key, $secret, $options);
 	}
 }
 
@@ -19,6 +19,25 @@ function user_api_information_form_submit($uid, $key, $secret){
 
 	$schoology = new SchoologyApi($key, $secret);
 
+	$output = '<body style="background-color:powderblue; padding:25px; font-family:Georgia;">';
+
+	if(isset($_POST['courses'])||isset($_POST['groups'])){
+		if($_POST['courses']) {
+			$output .= list_courses($uid, $schoology);
+		}
+		if($_POST['groups']) {
+			$output .= list_groups($uid, $schoology);
+		}
+	} else {
+		$output .= '<p>You did not select to list either groups or courses.</p>';
+	}
+
+	$output .='</body>';
+
+	print($output);
+}
+
+function list_courses($uid, $schoology) {
 	$api_result = $schoology->api('/users/' . $uid . '/sections');
 	$output = '<b>Courses</b>';
 	$output .= '<ul>';
@@ -27,7 +46,7 @@ function user_api_information_form_submit($uid, $key, $secret){
 	$has_courses = FALSE;
 	foreach($api_result->result->section as $section){
   		$has_courses = TRUE;
-  		$output .= '<li>' . $section->course_title . ':' . $section->section_title . '</li>';
+  		$output .= '<li>' . $section->course_title . ': ' . $section->section_title . '</li>';
 	}
  
 	// If no courses were found print an 'empty' message
@@ -37,7 +56,29 @@ function user_api_information_form_submit($uid, $key, $secret){
  
 	$output .= '</ul>';
 
-	print($output);
+	return $output;
+}
+
+function list_groups($uid, $schoology) {
+	$api_result = $schoology->api('/users/' . $uid . '/groups');
+	$output = '<b>Groups</b>';
+	$output .= '<ul>';
+ 
+	// Cycle through the result and print each group
+	$has_groups = FALSE;
+	foreach($api_result->result->group as $group){
+  		$has_groups = TRUE;
+  		$output .= '<li>' . $group->title;
+	}
+ 
+	// If no courses were found print an 'empty' message
+	if(!$has_groups){
+  		$output .= '<li>No groups were found for this user.</li>';
+	}
+ 
+	$output .= '</ul>';
+
+	return $output;
 }
 
 user_api_information_form_validate();
